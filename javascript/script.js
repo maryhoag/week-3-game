efb0e25dfbd11623d0a8d656417dde10bbfbb0da
Mary Hoag <marymcevoyhoag@gmail.com>
2016-04-21 18:13:17 -0400

    it kinda works now

--- javascript/script.js
+++ javascript/script.js
@@ -5,13 +5,13 @@ var cities = ["Paris", "Moscow", "Dublin", "Tokyo", "Madrid", "London" //, "Lisb
 ];
 
 var pictures = ['<a href="#"><img src="../assets/images/paris.jpeg" height="200" width="200" alt="Paris skyline"/></a>', '<a href="#"><img src="../assets/images/moscow.jpg"/></a>', "assets/images/dublin.jpeg", "assets/images/tokyo.jpeg", "assets/images/madrid.jpeg", "assets/images/london.jpeg"];
-
 var len = cities.length;
 
 var guesses = [];
 var ans, ansArray, userPick;
 var hiddenWord = [];
 var guessesRemaining = 12;
+var boolie = false;
 
 
 $("#images").append(pictures[1]);
@@ -42,23 +42,24 @@ var win = function() {
 
 //correct function
 //when the user picks correct letter, fill in the blank AND add to letters usesd
-var correct = function() {
+//var correct = function() {
     //fill in the blank
-    hiddenWord[i] = userPick;
-    $("#game").html(hiddenWord);
+    //hiddenWord[j] = userPick;
+    //$("#game").html(hiddenWord);
 
     //delete letter from ansArray
-    ansArray.splice(ans, 1);
-};
+    //ansArray.splice(ans, 1);
+//};
 
-var incorrect = function() {
+//var incorrect = function() {
     //add letter to guesses array
-    guesses.push(userPick);
+    //guesses.push(userPick);
     //put new array on screen
-    $("#guesses").append(guesses);
-    guessesRemaining--;
+    //$("#guesses").append(guesses);
+    //guessesRemaining--;
+    //$("#guessesRemaining").html(guessesRemaining);
 
-};
+//};
 
 
 //game begins
@@ -82,22 +83,28 @@ $(document).ready(function() {
 
                 //compare array
                 for (var j = 0; j <= len; j++) {
-                    document.onkeyup = function(event) {
-                    userPick = String.fromCharCode(event.keyCode).toLowerCase();
-                    
+
                     if (ans[j] == userPick) {
                         //replace underscore with correct letter
-                        correct();
+                        hiddenWord[j] = userPick;
+                        $("#game").html(hiddenWord);
+                        ansArray.splice(ans, 1);
+                        boolie = true;
+                    }
                         if (ansArray.length === 0) {
                             win();
                             break;
                         }
-                    } else {
                         //add wrong pick to used letters array and refresh onscreen
-                        incorrect();
+                    if(boolie == false) {
+                        guesses.push(userPick);
+                        //put new array on screen
+                        $("#guesses").append(guesses);
+                        guessesRemaining--;
+                        $("#guessesRemaining").html(guessesRemaining);
+                        boolie = false;
                     }
-                }
             };
         //}
-
+    }
 });
